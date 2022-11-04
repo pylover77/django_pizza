48726f4cb46e7325283a117bf291f1c55d8e08e5
pylover77 <ulquiorragx12@hotmail.com>
2022-11-04 19:19:22 -0300

    updating

--- Home/urls.py
+++ Home/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('orders/', views.orders, name="orders"),
     path('increament/', views.increament, name="increament"),
-    path('decreament/', views.decreament, name="decreament")
+    path('decreament/', views.decreament, name="decreament"),
+    path('deleteOrder/', views.deleteOrder, name="deleteOrder"),
+    path('deleteAll/', views.deleteAll, name="deleteAll" ),
+    path('orderConfirmed/', views.orderConfirmed, name="orderConfirmed" ),
+    path('profile/', views.profile, name="profile"),
 ]
--- Home/views.py
+++ Home/views.py
@@ -7,17 +7,61 @@ from django.contrib import messages
 from .models import Pizza, Orders
 # Create your views here.
 def home(request):
-    return render(request, 'Home/index.html')
+    if request.user and request.user != AnonymousUser():
+        current_user = request.user 
+        ordersOfCurrentUser = Orders.objects.filter(User=current_user)
+        
+        totalOrdersList = []
+        for pizzaOrder in ordersOfCurrentUser:
+            quantity = pizzaOrder.quantity
+            totalOrdersList.append(quantity)
+            
+        totalOrders = sum(totalOrdersList)
+    else:
+        totalOrders = 0
+    ordersOfCurrentUser = Orders.objects.filter(User=current_user)
+    firstobjectofcurrentuser = ordersOfCurrentUser.first()
+    #
+    context={
+    
+    'totalOrders':totalOrders,
+}
+    return render(request, 'Home/index.html', context)
 
 def orders(request):
     try:
         current_username = request.user.username
         user = User.objects.filter(username=current_username).first()
         orders = Orders.objects.filter(User=user)
-        
+        price_list = []
+        for order in orders:
+            price_list.append(order.Pizza_price)
+            
+        totalPrice = round(sum(price_list), 3)
+
+            
+        if request.user and request.user != AnonymousUser():
+            current_user = request.user 
+            ordersOfCurrentUser = Orders.objects.filter(User=current_user)
+            
+            totalOrdersList = []
+            for pizzaOrder in ordersOfCurrentUser:
+                quantity = pizzaOrder.quantity
+                totalOrdersList.append(quantity)
+                
+            totalOrders = sum(totalOrdersList)
+        else:
+            totalOrders = 0
+        ordersOfCurrentUser = Orders.objects.filter(User=current_user)
+        firstobjectofcurrentuser = ordersOfCurrentUser.first()
+        #
         context={
             'orders':orders,
+            'totalOrders':totalOrders,
+            'totalPrice':totalPrice,
+            'firstobjectofcurrentuser': firstobjectofcurrentuser,
         }
+        #
     except Exception:
         pass
     return render(request, "Home/orders.html", context)
@@ -102,6 +146,7 @@ def menu(request):
                         if not allOrders:
                             orders = Orders(Pizza_name = p_name, Pizza_desc = p_desc, Pizza_price = p_price, User = user)
                             orders.save()
+                            return redirect('menu')
                         else:
                             order_ = Orders.objects.filter(User=user, Pizza_name=p_name)
                             order_quantity = order_.first().quantity
@@ -147,7 +192,6 @@ def signup(request):
                     user_login = auth.authenticate(username=username, password=pass1)
                     if user_login is not None:
                         auth.login(request, user_login)
-                        print ('Você está Logado!')
                 else:
                     messages.warning(request, 'Senhas nao coincidem')
                     return redirect('signup')
@@ -164,9 +208,35 @@ def login(request):
                 user = auth.authenticate(username=user_name, password=user_pass)
                 if user is not None:
                     auth.login(request, user)
-                    print('login')
                     messages.success(request, 'Você está Logado')
                     return redirect('home')
     else:
         return redirect('home')
     return render(request, 'Home/login.html')
+
+def deleteOrder(request):
+    if request.method == "POST":
+        if 'order_Id' in request.POST:
+            order_Id = request.POST['order_Id']
+            current_user = request.user
+            current_order = Orders.objects.get(id=order_Id)
+            current_order.delete()
+            return redirect('orders')
+        
+
+def deleteAll(request):
+    if request.method == "POST":
+        current_user = request.user
+        current_order = Orders.objects.filter(User=current_user)
+        current_order.delete()
+        return redirect('orders')
+    
+def orderConfirmed(request):
+    if request.method == "POST":
+        current_user = request.user
+        allOrders = Orders.objects.filter(User=current_user).update(order_confirmed = True)
+        return redirect("orders")
+    
+
+def profile(request):
+    return render(request, "Home/profile.html")
